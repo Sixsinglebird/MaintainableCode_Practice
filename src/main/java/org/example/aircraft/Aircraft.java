de30dbd59e7b8ec3644c2eaee504caf790af9742
Alex Singleton <97618579+Sixsinglebird@users.noreply.github.com>
2023-11-09 10:50:31 -0330

    updated dependencies and Created aircraft class

--- src/main/java/org/example/aircraft/Aircraft.java
+++ src/main/java/org/example/aircraft/Aircraft.java
@@ -1,4 +1,72 @@
 package org.example.aircraft;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+
+@Entity
 public class Aircraft {
+
+    /**
+     * This assigns an id automagically to the aircraft.
+     * The javax.persistence.Entity gets mad if we do not have one.
+     */
+    @Id
+    @SequenceGenerator(name = "aircraft_sequence", sequenceName = "aircraft_sequence", allocationSize = 1, initialValue=1)
+    @GeneratedValue(generator = "aircraft_sequence")
+    private long id;
+
+    private String tailNumber;
+
+    private String type;
+
+    private String brand;
+
+    private String model;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+
+    public String getTailNumber() {
+        return tailNumber;
+    }
+
+    public void setTailNumber(String tailNumber) {
+        this.tailNumber = tailNumber;
+    }
+
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
 }
